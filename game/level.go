7c065cb32ccb48b46d1d2f386eba538d4f00ddff
droslean <droslean@gmail.com>
2016-07-12 23:02:26 +0200

    Add rooms

--- game/level.go
+++ game/level.go
@@ -5,7 +5,16 @@ type Level struct {
 	Tag   string `xml:"tag,attr"`
 	Name  string `xml:"name"`
 	Intro string `xml:"intro"`
-	Cubes []Cube `xml:"cubes>cube"`
+	Rooms []Room `xml:"rooms>room"`
+}
+
+type Room struct {
+	Id          string `xml:"id,attr"`
+	Name        string `xml:"name,attr"`
+	Description string `xml:"description,attr"`
+	Maxx        string `xml:"maxx,attr"`
+	Maxy        string `xml:"maxy,attr"`
+	Cubes       []Cube `xml:"cubes>cube"`
 }
 
 //TODO : Add Rooms in area file , Cubes must belong to a specific room, and many rooms belong to area.
