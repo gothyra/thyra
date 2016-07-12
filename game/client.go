bf816ed5e9f39c2c8954b53ef8005295805fea35
droslean <droslean@gmail.com>
2016-07-12 13:14:49 +0200

    Areas connected

--- game/client.go
+++ game/client.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -59,68 +60,84 @@ func (c Client) ReadLinesInto(ch chan<- string, server *Server) {
 		}
 
 		log.Printf("Command by %s: %s  -  %s", c.Player.Nickname, command, commandText)
-		mapname := c.Player.Area
-		map_array := populate_maparray(server, mapname)
 
-		printIntro(server, c, mapname)
+		map_array := populate_maparray(server, c.Player.Area)
+
+		printIntro(server, c, c.Player.Area)
 
 		switch command {
+		case "l":
+			fallthrough
 		case "look":
 			fallthrough
 		case "map":
-			findExits(map_array, c, c.Player.Position)
-			printExits(map_array, c, c.Player.Position)
-			updateMap(c, c.Player.Position, map_array)
+			posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
+			printToUser(server, c, map_array, posarray, c.Player.Area)
 		case "go":
 		case "exits":
-			log.Printf(c.Player.Position)
-			printExits(map_array, c, c.Player.Position)
+			posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
+			printToUser(server, c, map_array, posarray, c.Player.Area)
+
 		case "e":
 			fallthrough
 		case "east":
-			newpos := findExits(map_array, c, c.Player.Position)[0]
+			newpos, _ := strconv.Atoi(findExits(server, map_array, c, c.Player.Position, c.Player.Area)[0][1])
+			posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
 			if newpos > 0 {
+
 				c.Player.Position = strconv.Itoa(newpos)
-				updateMap(c, c.Player.Position, map_array)
-				findExits(map_array, c, c.Player.Position)
-				printExits(map_array, c, c.Player.Position)
+				c.Player.Area = posarray[0][0]
+				map_array := populate_maparray(server, c.Player.Area)
+				posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
+				printToUser(server, c, map_array, posarray, c.Player.Area)
 			} else {
 				c.WriteToUser("You can't go that way\n")
 			}
 		case "w":
 			fallthrough
 		case "west":
-			newpos := findExits(map_array, c, c.Player.Position)[1]
+			newpos, _ := strconv.Atoi(findExits(server, map_array, c, c.Player.Position, c.Player.Area)[1][1])
+			posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
 			if newpos > 0 {
+
 				c.Player.Position = strconv.Itoa(newpos)
-				updateMap(c, c.Player.Position, map_array)
-				findExits(map_array, c, c.Player.Position)
-				printExits(map_array, c, c.Player.Position)
+				c.Player.Area = posarray[1][0]
+				map_array := populate_maparray(server, c.Player.Area)
+				posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
+				printToUser(server, c, map_array, posarray, c.Player.Area)
+
 			} else {
 				c.WriteToUser("You can't go that way\n")
 			}
 		case "n":
 			fallthrough
 		case "north":
-			newpos := findExits(map_array, c, c.Player.Position)[2]
+			newpos, _ := strconv.Atoi(findExits(server, map_array, c, c.Player.Position, c.Player.Area)[2][1])
+			posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
 			if newpos > 0 {
+
 				c.Player.Position = strconv.Itoa(newpos)
-				updateMap(c, c.Player.Position, map_array)
-				findExits(map_array, c, c.Player.Position)
-				printExits(map_array, c, c.Player.Position)
+				c.Player.Area = posarray[2][0]
+				map_array := populate_maparray(server, c.Player.Area)
+				posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
+				printToUser(server, c, map_array, posarray, c.Player.Area)
+
 			} else {
 				c.WriteToUser("You can't go that way\n")
 			}
 		case "s":
 			fallthrough
 		case "south":
-
-			newpos := findExits(map_array, c, c.Player.Position)[3]
+			newpos, _ := strconv.Atoi(findExits(server, map_array, c, c.Player.Position, c.Player.Area)[3][1])
+			posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
 			if newpos > 0 {
+
 				c.Player.Position = strconv.Itoa(newpos)
-				updateMap(c, c.Player.Position, map_array)
-				findExits(map_array, c, c.Player.Position)
-				printExits(map_array, c, c.Player.Position)
+				c.Player.Area = posarray[3][0]
+				map_array := populate_maparray(server, c.Player.Area)
+				posarray := findExits(server, map_array, c, c.Player.Position, c.Player.Area)
+				printToUser(server, c, map_array, posarray, c.Player.Area)
+
 			} else {
 				c.WriteToUser("You can't go that way\n")
 			}
@@ -136,10 +153,11 @@ func (c Client) ReadLinesInto(ch chan<- string, server *Server) {
 				c.WriteToUser(nickname + "\n")
 			}
 		default:
-
+			c.WriteLineToUser("Huh?")
 			continue
 
 			c.WriteToUser("\033[1F\033[K")
+
 		}
 	}
 }
@@ -153,57 +171,97 @@ func (c Client) WriteLinesFrom(ch <-chan string) {
 	}
 }
 
-func findExits(s [][]int, c Client, pos string) [4]int { //Briskei ta exits analoga me to position tou user  ,default possition otan kanei register einai 1
+func findExits(server *Server, s [][]int, c Client, pos string, area string) [][]string { //Briskei ta exits analoga me to position tou user  ,default possition otan kanei register einai 1
 	intpos, _ := strconv.Atoi(pos) //to exw balei etsi prwsorina.
-	exitarr := [4]int{0, 0, 0, 0}  // 2d array s   ,einai o xartis.
+	exitarr := [][]string{}        // 2d array s   ,einai o xartis.
+
+	east := []string{area, "0"}
+	west := []string{area, "0"}
+	north := []string{area, "0"}
+	south := []string{area, "0"}
+
+	exitarr = append(exitarr, east)
+	exitarr = append(exitarr, west)
+	exitarr = append(exitarr, north)
+	exitarr = append(exitarr, south)
+
+	areaCubes := server.levels[area].Cubes
+
 	for x := 0; x < len(s); x++ {
 		for y := 0; y < len(s); y++ { // Kanei return ena array [4]int me ta exits se morfi  {EAST , WEST , NORTH , SOUTH}
 			if s[x][y] == intpos { //P.X an kanei return  {50,0,40,0} simainei oti apo to possition pou eisai exei exits EAST kai NORTH
 				// EAST se paei sto cube me ID 50 kai NORTH se paei sto cube me ID 40
 
 				if x < len(s)-1 && s[x+1][y] > 0 {
-					exitarr[0] = s[x+1][y]
+					exitarr[0][1] = strconv.Itoa((s[x+1][y])) //EAST
 				}
 				if x > 0 && s[x-1][y] > 0 {
-					exitarr[1] = s[x-1][y]
+					exitarr[1][1] = strconv.Itoa(s[x-1][y]) //WEST
 				}
 				if y > 0 && s[x][y-1] > 0 {
-					exitarr[2] = s[x][y-1]
+					exitarr[2][1] = strconv.Itoa(s[x][y-1]) //NORTH
 				}
 				if y < len(s)-1 && s[x][y+1] > 0 {
-					exitarr[3] = s[x][y+1]
+					exitarr[3][1] = strconv.Itoa(s[x][y+1]) //SOUTH
 				}
 			}
 
 		}
+	}
+	//Finding Exits that belongs to different area.
+	for i := range areaCubes {
+
+		if areaCubes[i].ID == pos {
+			if areaCubes[i].ToId != "" && areaCubes[i].FromExit == "EAST" {
+				exitarr[0][1] = areaCubes[i].ToId
+				exitarr[0][0] = areaCubes[i].ToArea
+
+			}
+
+			if areaCubes[i].ToId != "" && areaCubes[i].FromExit == "WEST" {
+				exitarr[1][1] = areaCubes[i].ToId
+				exitarr[1][0] = areaCubes[i].ToArea
+
+			}
+
+			if areaCubes[i].ToId != "" && areaCubes[i].FromExit == "NORTH" {
+				exitarr[2][1] = areaCubes[i].ToId
+				exitarr[2][0] = areaCubes[i].ToArea
+
+			}
+
+			if areaCubes[i].ToId != "" && areaCubes[i].FromExit == "SOUTH" {
+				exitarr[3][1] = areaCubes[i].ToId
+				exitarr[3][0] = areaCubes[i].ToArea
+
+			}
+		}
+
 	}
 	c.WriteToUser("\n")
 	return exitarr
 }
 
-func printExits(s [][]int, c Client, pos string) { //akribws idio func me to findExits , apla edw kanw print ston user.
-	intpos, _ := strconv.Atoi(pos)
-	c.WriteToUser("Exits  : ")
-	for x := 0; x < len(s); x++ {
-		for y := 0; y < len(s); y++ {
-			if s[x][y] == intpos {
-				if x < len(s)-1 && s[x+1][y] > 0 {
-					c.WriteToUser("East ")
-				}
-				if x > 0 && s[x-1][y] > 0 {
-					c.WriteToUser("West ")
-				}
-				if y > 0 && s[x][y-1] > 0 {
-					c.WriteToUser("North ")
-				}
-				if y < len(s)-1 && s[x][y+1] > 0 {
-					c.WriteToUser("South ")
-				}
-			}
+func printExits(c Client, exit_array [][]string) bytes.Buffer { //Print exits,From returned [5]string findExits
 
-		}
+	var buffer bytes.Buffer
+	buffer.WriteString("Exits  : [ ")
+
+	if exit_array[0][1] != "0" {
+		buffer.WriteString("East ")
 	}
-	c.WriteToUser("\n")
+
+	if exit_array[1][1] != "0" {
+		buffer.WriteString("West ")
+	}
+	if exit_array[2][1] != "0" {
+		buffer.WriteString("North ")
+	}
+	if exit_array[3][1] != "0" {
+		buffer.WriteString("South ")
+	}
+	buffer.WriteString("]\n")
+	return buffer
 
 }
 
@@ -257,33 +315,51 @@ func populate_maparray(s *Server, area string) [][]int {
 	return maparray
 }
 
-func updateMap(c Client, pos string, s [][]int) { //Print ton xarti
+func updateMap(server *Server, c Client, pos string, s [][]int, posarray [][]string) bytes.Buffer { //Print ton xarti
 
+	var buffer bytes.Buffer
 	intpos, _ := strconv.Atoi(pos)
+
+	//TODO : Print "}" when the exit goes to different room-area.
 	for x := 0; x < len(s); x++ {
-		c.WriteToUser("|")
+
+		buffer.WriteString("|")
 
 		for y := 0; y < len(s); y++ {
 
 			if s[y][x] != 0 && s[y][x] != intpos {
-				c.WriteToUser("___|")
+				buffer.WriteString("___|")
 			} else if s[y][x] == intpos {
 
-				c.WriteToUser("_*_|")
+				buffer.WriteString("_*_|")
 			} else {
-				c.WriteToUser("_X_|")
+				buffer.WriteString("XXX|")
 
 			}
-
 		}
-		c.WriteToUser("\n")
+
+		buffer.WriteString("\n")
 	}
+
+	return buffer
 }
 
-func printIntro(s *Server, c Client, area string) { // Print to intro tis area
+func printIntro(s *Server, c Client, area string) bytes.Buffer { // Print to intro tis area
+
+	var buffer bytes.Buffer
 
 	areaIntro := s.levels[area].Intro
+	buffer.WriteString(areaIntro)
 
-	c.WriteLineToUser(areaIntro)
+	return buffer
+}
+
+func printToUser(s *Server, c Client, map_array [][]int, posarray [][]string, areaname string) {
+
+	buffexits := printExits(c, posarray)
+	buff := updateMap(s, c, c.Player.Position, map_array, posarray)
+	buffintro := printIntro(s, c, areaname)
+
+	c.WriteToUser(buffintro.String() + "\n\n" + buffexits.String() + "\n\n" + buff.String())
 
 }
