1d5b18636cbf181fac581cd48b85fc2705789b35
Nikolaos Leandros Moraitis <droslean@gmail.com>
2016-09-07 15:56:16 +0200

    Fixing Exits

--- game/server.go
+++ game/server.go
@@ -429,6 +429,30 @@ func (s *Server) HandleCommand(c Client, command string, roomsMap map[string]map
 	case "where":
 		updateMap(s, c, c.Player.Position, map_array)
 
+	case "list":
+		for i := range map_array {
+			for y := range map_array {
+
+				if map_array[y][i].ID != "" {
+					id, _ := strconv.Atoi(map_array[y][i].ID)
+					if id < 10 {
+						c.WriteToUser("|  " + map_array[y][i].ID + " |")
+
+					} else {
+						c.WriteToUser("| " + map_array[y][i].ID + " |")
+
+					}
+
+				} else {
+					c.WriteToUser("| XX |")
+
+				}
+
+			}
+			c.WriteToUser("\n")
+
+		}
+
 	default:
 		c.WriteLineToUser("Huh?")
 	}
@@ -449,15 +473,15 @@ func (server *Server) FindExits(s [][]Cube, area, room, pos string) [][]string {
 	exitarr = append(exitarr, north)
 	exitarr = append(exitarr, south)
 
-	roomCubes := server.Areas[area].Rooms[room].Cubes
+	//roomCubes := server.Areas[area].Rooms[room].Cubes
 
 	east_id := 0
 	west_id := 0
 	north_id := 0
 	south_id := 0
 
-	for x := 0; x < len(s); x++ {
-		for y := 0; y < len(s); y++ {
+	for y := 0; y < len(s); y++ {
+		for x := 0; x < len(s); x++ {
 			if s[x][y].ID == pos {
 
 				if x < len(s)-1 {
@@ -467,60 +491,63 @@ func (server *Server) FindExits(s [][]Cube, area, room, pos string) [][]string {
 					west_id, _ = strconv.Atoi(s[x-1][y].ID)
 				}
 				if y > 0 {
-					north_id, _ = strconv.Atoi(s[y-1][y].ID)
+
+					north_id, _ = strconv.Atoi(s[x][y-1].ID)
 				}
 				if y < len(s)-1 {
-					south_id, _ = strconv.Atoi(s[y+1][y].ID)
+					south_id, _ = strconv.Atoi(s[x][y+1].ID)
 
 				}
 
 				if east_id > 0 {
-					exitarr[0][1] = s[x+1][y].ID //EAST
+
+					if s[x+1][y].Type == "door" {
+						exitarr[0][0] = s[x+1][y].Exits[0].ToArea
+						exitarr[0][1] = s[x+1][y].Exits[0].ToCubeID
+						exitarr[0][2] = s[x+1][y].Exits[0].ToRoom
+					} else {
+						exitarr[0][1] = s[x+1][y].ID //EAST
+
+					}
 				}
+
 				if west_id > 0 {
-					exitarr[1][1] = s[x-1][y].ID //WEST
+
+					if s[x-1][y].Type == "door" {
+						exitarr[1][0] = s[x-1][y].Exits[0].ToArea
+						exitarr[1][1] = s[x-1][y].Exits[0].ToCubeID
+						exitarr[1][2] = s[x-1][y].Exits[0].ToRoom
+					} else {
+
+						exitarr[1][1] = s[x-1][y].ID //WEST
+					}
 				}
+
 				if north_id > 0 {
-					exitarr[2][1] = s[x][y-1].ID //NORTH
+
+					if s[x][y-1].Type == "door" {
+						exitarr[2][0] = s[x][y-1].Exits[0].ToArea
+						exitarr[2][1] = s[x][y-1].Exits[0].ToCubeID
+						exitarr[2][2] = s[x][y-1].Exits[0].ToRoom
+					} else {
+						exitarr[2][1] = s[x][y-1].ID //NORTH
+					}
 				}
+
 				if south_id > 0 {
-					exitarr[3][1] = s[x][y+1].ID //SOUTH
+
+					if s[x][y+1].Type == "door" {
+						exitarr[3][0] = s[x][y+1].Exits[0].ToArea
+						exitarr[3][1] = s[x][y+1].Exits[0].ToCubeID
+						exitarr[3][2] = s[x][y+1].Exits[0].ToRoom
+					} else {
+
+						exitarr[3][1] = s[x][y+1].ID //SOUTH
+					}
+
 				}
 			}
 
-		}
-	}
-
-	//Finding Exits that belongs to different area or room.
-	for i := range roomCubes {
-		if roomCubes[i].ID == pos {
-			for z := range roomCubes[i].Exits {
-				if roomCubes[i].Exits[z].ToCubeID == "" {
-					continue
-				}
-
-				switch roomCubes[i].Exits[z].FromExit {
-				case "East":
-					exitarr[0][0] = roomCubes[i].Exits[z].ToArea
-					exitarr[0][1] = roomCubes[i].Exits[z].ToCubeID
-					exitarr[0][2] = roomCubes[i].Exits[z].ToRoom
-
-				case "West":
-					exitarr[1][0] = roomCubes[i].Exits[z].ToArea
-					exitarr[1][1] = roomCubes[i].Exits[z].ToCubeID
-					exitarr[1][2] = roomCubes[i].Exits[z].ToRoom
-
-				case "North":
-					exitarr[2][0] = roomCubes[i].Exits[z].ToArea
-					exitarr[2][1] = roomCubes[i].Exits[z].ToCubeID
-					exitarr[2][2] = roomCubes[i].Exits[z].ToRoom
-
-				case "South":
-					exitarr[3][0] = roomCubes[i].Exits[z].ToArea
-					exitarr[3][1] = roomCubes[i].Exits[z].ToCubeID
-					exitarr[3][2] = roomCubes[i].Exits[z].ToRoom
-				}
-			}
 		}
 	}
 
@@ -528,6 +555,8 @@ func (server *Server) FindExits(s [][]Cube, area, room, pos string) [][]string {
 	// [0] East, [1] West, [2] North, [3] South
 	// Second array holds the cube we will end up following the direction
 	// [][0] ToArea, [][1] ToCubeID, [][2] ToRoom
+
+	//	fmt.Printf("%v", exitarr)
 
 	return exitarr
 }
