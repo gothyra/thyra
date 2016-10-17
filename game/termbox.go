3187a4430516b47f883fd6fdbfe768475e177487
Nikolaos Leandros Moraitis <droslean@gmail.com>
2016-10-17 14:35:14 +0200

    Cleanup

--- game/termbox.go
+++ game/termbox.go
@@ -5,33 +5,10 @@ package game
 import (
 	"fmt"
 	"io"
-	"os"
 	"strconv"
-	"syscall"
 	"unicode/utf8"
-	"unsafe"
 
 	log "gopkg.in/inconshreveable/log15.v2"
-)
-
-// private API
-
-const (
-	t_enter_ca = iota
-	t_exit_ca
-	t_show_cursor
-	t_hide_cursor
-	t_clear_screen
-	t_sgr0
-	t_underline
-	t_bold
-	t_blink
-	t_reverse
-	t_enter_keypad
-	t_exit_keypad
-	t_enter_mouse
-	t_exit_mouse
-	t_max_funcs
 )
 
 const (
@@ -39,48 +16,21 @@ const (
 	attr_invalid  = Attribute(0xFFFF)
 )
 
-type input_event struct {
-	data []byte
-	err  error
-}
-
 var (
 	// term specific sequences
 	keys  []string
 	funcs []string
 
-	// termbox inner state
-	//orig_tios    syscall_Termios
-	back_buffer  Cellbuf
-	front_buffer Cellbuf
-	termw        int
-	termh        int
-	//input_mode  = InputEsc
-	//output_mode = OutputNormal
-	out        *os.File
-	in         int
-	lastfg     = attr_invalid
-	lastbg     = attr_invalid
+	termw int
+	termh int
+
 	lastx      = coord_invalid
 	lasty      = coord_invalid
 	cursor_x   = cursor_hidden
 	cursor_y   = cursor_hidden
 	foreground = ColorDefault
 	background = ColorDefault
-	//	inbuf      = make([]byte, 0, 64)
-	//outbuf         Client.Buff
-	//sigwinch       = make(chan os.Signal, 1)
-	//sigio          = make(chan os.Signal, 1)
-	quit           = make(chan int)
-	input_comm     = make(chan input_event)
-	interrupt_comm = make(chan struct{})
-	intbuf         = make([]byte, 0, 16)
-
-	// grayscale indexes
-	grayscale = []Attribute{
-		0, 17, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244,
-		245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 232,
-	}
+	intbuf     = make([]byte, 0, 16)
 )
 
 func write_cursor(x, y int, c Client) {
@@ -91,20 +41,6 @@ func write_cursor(x, y int, c Client) {
 	c.Buff.WriteString("H")
 
 	io.WriteString(c.Conn, c.Buff.String())
-}
-
-type winsize struct {
-	rows    uint16
-	cols    uint16
-	xpixels uint16
-	ypixels uint16
-}
-
-func get_term_size(fd uintptr) (int, int) {
-	var sz winsize
-	_, _, _ = syscall.Syscall(syscall.SYS_IOCTL,
-		fd, uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&sz)))
-	return int(sz.cols), int(sz.rows)
 }
 
 func send_char(x, y int, ch rune, c Client) {
