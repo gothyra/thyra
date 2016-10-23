341cfbfe022a3a09d6c2f2bdbd038a8a27850254
Michail Kargakis <mkargaki@redhat.com>
2016-10-23 12:32:03 +0200

    Simplify code a bit, add some comments

--- pkg/server/god.go
+++ pkg/server/god.go
@@ -11,14 +11,17 @@ import (
 	"github.com/gothyra/thyra/pkg/client"
 )
 
-func God(
-	s *Server,
-	wg *sync.WaitGroup,
-	quit <-chan struct{},
-	map_array map[string]map[string][][]area.Cube,
-) {
+func God(s *Server, wg *sync.WaitGroup, quit <-chan struct{}) {
 	log.Info("god started")
 	defer wg.Done()
+
+	roomsMap := make(map[string]map[string][][]area.Cube)
+	for _, a := range s.Areas {
+		roomsMap[a.Name] = make(map[string][][]area.Cube)
+		for _, room := range a.Rooms {
+			roomsMap[a.Name][room.Name] = s.CreateRoom(a.Name, room.Name)
+		}
+	}
 
 	for {
 		select {
@@ -33,41 +36,41 @@ func God(
 			switch ev.Etype {
 			case "look":
 				wg.Add(1)
-				godPrintRoom(s, *cl, c, wg, quit, map_array, "", "")
+				godPrintRoom(s, *cl, c, wg, quit, roomsMap, "", "")
 
 			case "move_east":
-				msg := doMove(s, *cl, map_array, 0)
+				msg := doMove(s, *cl, roomsMap, 0)
 				wg.Add(1)
-				godPrintRoom(s, *cl, c, wg, quit, map_array, msg, "")
+				godPrintRoom(s, *cl, c, wg, quit, roomsMap, msg, "")
 
 			case "move_west":
-				msg := doMove(s, *cl, map_array, 1)
+				msg := doMove(s, *cl, roomsMap, 1)
 				wg.Add(1)
-				godPrintRoom(s, *cl, c, wg, quit, map_array, msg, "")
+				godPrintRoom(s, *cl, c, wg, quit, roomsMap, msg, "")
 
 			case "move_north":
-				msg := doMove(s, *cl, map_array, 2)
+				msg := doMove(s, *cl, roomsMap, 2)
 				wg.Add(1)
-				godPrintRoom(s, *cl, c, wg, quit, map_array, msg, "")
+				godPrintRoom(s, *cl, c, wg, quit, roomsMap, msg, "")
 
 			case "move_south":
-				msg := doMove(s, *cl, map_array, 3)
+				msg := doMove(s, *cl, roomsMap, 3)
 				wg.Add(1)
-				godPrintRoom(s, *cl, c, wg, quit, map_array, msg, "")
+				godPrintRoom(s, *cl, c, wg, quit, roomsMap, msg, "")
 			case "enter_door":
 				currentroom := s.OnlineClientsGetByRoom(cl.Player.Area, cl.Player.Room)
 				wg.Add(1)
-				godPrintRoom(s, *cl, currentroom, wg, quit, map_array, "", fmt.Sprintf("%s enter the room.", cl.Player.Nickname))
+				godPrintRoom(s, *cl, currentroom, wg, quit, roomsMap, "", fmt.Sprintf("%s enter the room.", cl.Player.Nickname))
 
 				previousroom := s.OnlineClientsGetByRoom(cl.Player.PreviousArea, cl.Player.PreviousRoom)
 				if previousroom != nil {
 					wg.Add(1)
-					godPrintRoom(s, *cl, previousroom, wg, quit, map_array, "", fmt.Sprintf("%s left the room.", cl.Player.Nickname))
+					godPrintRoom(s, *cl, previousroom, wg, quit, roomsMap, "", fmt.Sprintf("%s left the room.", cl.Player.Nickname))
 				}
 
 			case "quit":
 				//TODO :
-				//godPrint(s, c, wg, quit, map_array, fmt.Sprintf("%s has quit.", c.Player.Nickname))
+				//godPrint(s, c, wg, quit, roomsMap, fmt.Sprintf("%s has quit.", c.Player.Nickname))
 				//clients := s.OnlineClientsGetByRoom(c.Player.Area, c.Player.Room)
 				//for i := range clients {
 				//	log.Info(fmt.Sprintf("Clients same room : %s", clients[i].Player.Nickname))
@@ -77,7 +80,7 @@ func God(
 
 			case "unknown":
 				wg.Add(1)
-				godPrintRoom(s, *cl, c, wg, quit, map_array, "Huh?", "")
+				godPrintRoom(s, *cl, c, wg, quit, roomsMap, "Huh?", "")
 
 			}
 		}
@@ -92,13 +95,13 @@ func godPrintRoom(
 	quit <-chan struct{},
 	roomsMap map[string]map[string][][]area.Cube,
 	msg string,
-	global_msg string,
+	globalMsg string,
 ) {
 	defer wg.Done()
 
 	positionToCurrent := map[string]bool{}
 
-	map_array := roomsMap[clients[0].Player.Area][clients[0].Player.Room]
+	mapArray := roomsMap[clients[0].Player.Area][clients[0].Player.Room]
 	for i := range clients {
 		c := clients[i]
 		positionToCurrent[c.Player.Position] = false
@@ -111,8 +114,8 @@ func godPrintRoom(
 		posToCurr := copyMapWithNewPos(positionToCurrent, c.Player.Position)
 
 		buffintro := area.PrintIntro(s.Areas[c.Player.Area].Rooms[c.Player.Room].Description)
-		bufmap := area.PrintMap(p, posToCurr, map_array)
-		bufexits := area.PrintExits(area.FindExits(map_array, c.Player.Area, c.Player.Room, c.Player.Position))
+		bufmap := area.PrintMap(p, posToCurr, mapArray)
+		bufexits := area.PrintExits(area.FindExits(mapArray, c.Player.Area, c.Player.Room, c.Player.Position))
 
 		reply := client.Reply{
 			World: bufmap.Bytes(),
@@ -123,7 +126,7 @@ func godPrintRoom(
 		if cl.Player.Nickname == p.Nickname {
 			reply.Events = msg
 		} else {
-			reply.Events = global_msg
+			reply.Events = globalMsg
 		}
 
 		select {
@@ -151,11 +154,11 @@ func doMove(s *Server, c client.Client, roomsMap map[string]map[string][][]area.
 		Client: &c,
 	}
 
-	map_array := roomsMap[c.Player.Area][c.Player.Room]
-	posarray := area.FindExits(map_array, c.Player.Area, c.Player.Room, c.Player.Position)
+	mapArray := roomsMap[c.Player.Area][c.Player.Room]
+	posarray := area.FindExits(mapArray, c.Player.Area, c.Player.Room, c.Player.Position)
 
-	newpos_type := posarray[direction][3]
-	if newpos_type == "door" {
+	newPosType := posarray[direction][3]
+	if newPosType == "door" {
 		event.Etype = "enter_door"
 		s.Events <- event
 	}
@@ -163,10 +166,9 @@ func doMove(s *Server, c client.Client, roomsMap map[string]map[string][][]area.
 	newarea := posarray[direction][0]
 	newroom := posarray[direction][2]
 	newpos, _ := strconv.Atoi(posarray[direction][1])
-	is_avail, pexist := isCubeAvailable(s, c, newarea, newroom, newpos)
+	isAvailable, info := isCubeAvailable(s, c, newarea, newroom, newpos)
 
-	if is_avail {
-
+	if isAvailable {
 		c.Player.PreviousArea = c.Player.Area
 		c.Player.PreviousRoom = c.Player.Room
 		c.Player.Position = strconv.Itoa(newpos)
@@ -175,10 +177,12 @@ func doMove(s *Server, c client.Client, roomsMap map[string]map[string][][]area.
 		return ""
 	}
 
-	return pexist
+	return info
 
 }
 
+// isCubeAvailable returns if the given cube is available, otherwise includes info about what or who is
+// occupying it.
 func isCubeAvailable(s *Server, client client.Client, area string, room string, cube int) (bool, string) {
 
 	if cube <= 0 {
@@ -188,10 +192,8 @@ func isCubeAvailable(s *Server, client client.Client, area string, room string, 
 	online := s.OnlineClients()
 	for i := range online {
 		c := online[i]
-		if c.Player.Area == area &&
-			c.Player.Room == room &&
-			c.Player.Position == strconv.Itoa(cube) &&
-			client.Player.Nickname != c.Player.Nickname {
+
+		if c.Player.Area == area && c.Player.Room == room && c.Player.Position == strconv.Itoa(cube) && client.Player.Nickname != c.Player.Nickname {
 			return false, c.Player.Nickname + " is blocking the way"
 		}
 	}
