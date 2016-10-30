fa3cad8ca6cfa4a2883dadb7827e841fec04bf39
Michail Kargakis <mkargaki@redhat.com>
2016-10-30 14:49:40 +0100

    Move server init code in s.Start

--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
-	"io"
-	"net"
 	"os"
-	"os/signal"
-	"path/filepath"
-	"sync"
-	"time"
 
 	log "gopkg.in/inconshreveable/log15.v2"
 	"gopkg.in/inconshreveable/log15.v2/stack"
 
-	"github.com/gothyra/thyra/pkg/client"
 	"github.com/gothyra/thyra/pkg/server"
 )
 
@@ -45,237 +37,14 @@ func customFormat() log.Format {
 func init() {
 	h := log.StreamHandler(os.Stdout, customFormat())
 	log.Root().SetHandler(h)
+	flag.Parse()
 }
+
+// Flags
+var port = flag.Int64("port", 4000, "Port to listen on incoming connections")
 
 func main() {
-	// Environment variables
-	staticDir := os.Getenv("THYRA_STATIC")
-	if len(staticDir) == 0 {
-		pwd, _ := os.Getwd()
-		staticDir = filepath.Join(pwd, "static")
-		log.Warn("Set THYRA_STATIC if you wish to configure the directory for static content")
-	}
-	log.Info(fmt.Sprintf("Using %s for static content", staticDir))
-
-	// Flags
-	port := flag.Int64("port", 4000, "Port to listen on incoming connections")
-	flag.Parse()
-
 	// Setup and start the server
-	s := server.NewServer(staticDir)
-
-	if err := s.LoadConfig(); err != nil {
-		os.Exit(1)
-	}
-
-	if err := s.LoadAreas(); err != nil {
-		os.Exit(1)
-	}
-
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		log.Info(err.Error())
-		os.Exit(1)
-	}
-	log.Info(fmt.Sprintf("Listen on: %s", ln.Addr()))
-
-	wg := &sync.WaitGroup{}
-	quit := make(chan struct{})
-	regRequest := make(chan client.LoginRequest, 1000)
-	clientRequest := make(chan client.Request, 1000)
-
-	wg.Add(1)
-	go handleRegistrations(*s, wg, quit, regRequest)
-
-	wg.Add(1)
-	go acceptConnections(ln, s, wg, quit, clientRequest, regRequest)
-
-	wg.Add(1)
-	go broadcast(*s, wg, quit, clientRequest)
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt, os.Kill)
-	select {
-	case <-signals:
-		log.Warn("Server is terminating...")
-		close(quit)
-	}
-
-	wg.Wait()
-	log.Warn("Server shutdown.")
-}
-
-// handleRegistrations accepts requests for registration and replies back if the requested
-// username exists or not.
-func handleRegistrations(s server.Server, wg *sync.WaitGroup, quit chan struct{}, regRequest chan client.LoginRequest) {
-	log.Info("handleRegistrations started")
-	defer wg.Done()
-
-	for {
-		exists := false
-		var err error
-
-		select {
-		case <-quit:
-			log.Warn("handleRegistrations quit")
-			return
-		case request := <-regRequest:
-			exists, err = s.LoadPlayer(request.Username)
-			if err != nil {
-				io.WriteString(request.Conn, fmt.Sprintf("%s\n", err.Error()))
-				continue
-			}
-
-			select {
-			case request.Reply <- exists:
-			case <-quit:
-				log.Warn("handleRegistrations quit")
-				return
-			}
-
-		}
-	}
-}
-
-func acceptConnections(
-	ln net.Listener,
-	s *server.Server,
-	wg *sync.WaitGroup,
-	quit <-chan struct{},
-	clientCh chan<- client.Request,
-	regRequest chan client.LoginRequest,
-) {
-	log.Info("acceptConnections started")
-	defer wg.Done()
-
-	for {
-		select {
-		case <-quit:
-			log.Warn("acceptConnections quit")
-			return
-		default:
-		}
-
-		ln.(*net.TCPListener).SetDeadline(time.Now().Add(10 * time.Second))
-		conn, err := ln.Accept()
-		if err != nil {
-			if opErr, ok := err.(*net.OpError); !ok || !opErr.Timeout() {
-				log.Info(err.Error())
-			}
-			continue
-		}
-
-		// TODO: handleConnection is not terminating gracefully right now because it blocks on waiting
-		// ReadLinesInto to quit which in turn is blocked on user input.
-		go handleConnection(conn, s, wg, quit, clientCh, regRequest)
-	}
-}
-
-// handleConnection should be invoked as a goroutine.
-func handleConnection(
-	conn net.Conn,
-	s *server.Server,
-	wg *sync.WaitGroup,
-	quit <-chan struct{},
-	clientCh chan<- client.Request,
-	regRequest chan<- client.LoginRequest,
-) {
-	log.Info("handleConnection started")
-	defer wg.Done()
-
-	bufc := bufio.NewReader(conn)
-	defer conn.Close()
-
-	log.Info(fmt.Sprintf("New connection open: %s", conn.RemoteAddr()))
-
-	io.WriteString(conn, welcomePage)
-
-	var username string
-	questions := 0
-
-out:
-	for {
-		if questions >= 3 {
-			io.WriteString(conn, "See you\n")
-			return
-		}
-
-		username = promptMessage(conn, bufc, "Whats your Nick?\n")
-		isValidName := server.IsValidUsername(username)
-		if !isValidName {
-			questions++
-			io.WriteString(conn, fmt.Sprintf("Username %s is not valid (0-9a-z_-).\n", username))
-			continue
-		}
-
-		exists := false
-		replyCh := make(chan bool, 1)
-
-		select {
-		case regRequest <- client.LoginRequest{Username: username, Conn: conn, Reply: replyCh}:
-		case <-quit:
-			return
-		}
-
-		select {
-		case exists = <-replyCh:
-		case <-quit:
-			return
-		}
-
-		if exists {
-			break out
-		}
-
-		questions++
-		io.WriteString(conn, fmt.Sprintf("Username %s does not exists.\n", username))
-		answer := promptMessage(conn, bufc, "Do you want to create that user? [y|n] ")
-
-		if answer == "y" || answer == "yes" {
-			s.CreatePlayer(username)
-			break
-		}
-	}
-
-	player, _ := s.GetPlayerByNick(username)
-	c := client.NewClient(conn, &player, clientCh)
-	log.Info(fmt.Sprintf("Player %q got connected", c.Player.Nickname))
-	s.ClientLoggedIn(c.Player.Nickname, *c)
-
-	wg.Add(1)
-	go c.Redraw(wg, quit)
-
-	// TODO: Main client thread is not terminating gracefully right now because it blocks on waiting
-	// for the user to hit Enter before proceeding to check for quit.
-	c.ReadLinesInto(quit)
-	log.Info(fmt.Sprintf("Connection from %v closed.", conn.RemoteAddr()))
-}
-
-func promptMessage(c net.Conn, bufc *bufio.Reader, message string) string {
-	for {
-		io.WriteString(c, message)
-		answer, _, _ := bufc.ReadLine()
-		if string(answer) != "" {
-			return string(answer)
-		}
-	}
-}
-
-// TODO: Maybe parallelize this so that each client request is handled on a separate routine.
-func broadcast(s server.Server, wg *sync.WaitGroup, quit <-chan struct{}, reqChan <-chan client.Request) {
-	log.Info("broadcast started")
-	defer wg.Done()
-
-	wg.Add(1)
-	go server.God(&s, wg, quit)
-
-	for {
-		select {
-		case request := <-reqChan:
-			s.HandleCommand(*request.Client, request.Cmd)
-		case <-quit:
-			log.Warn("broadcast quit")
-			return
-		}
-	}
+	s := server.NewServer()
+	s.Start(*port)
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,14 +1,19 @@
 package server
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"regexp"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/gothyra/toml"
 	log "gopkg.in/inconshreveable/log15.v2"
@@ -37,19 +42,38 @@ type Server struct {
 }
 
 // NewServer creates a new Server.
-func NewServer(staticDir string) *Server {
-	return &Server{
+func NewServer() *Server {
+	// Environment variables
+	staticDir := os.Getenv("THYRA_STATIC")
+	if len(staticDir) == 0 {
+		pwd, _ := os.Getwd()
+		staticDir = filepath.Join(pwd, "static")
+		log.Warn("Set THYRA_STATIC if you wish to configure the directory for static content")
+	}
+	log.Info(fmt.Sprintf("Using %s for static content", staticDir))
+
+	s := &Server{
 		Players:       make(map[string]area.Player),
 		onlineClients: make(map[string]*client.Client),
 		Areas:         make(map[string]area.Area),
 		staticDir:     staticDir,
 		Events:        make(chan client.Event, 1000),
 	}
+
+	if err := s.loadConfig(); err != nil {
+		os.Exit(1)
+	}
+
+	if err := s.loadAreas(); err != nil {
+		os.Exit(1)
+	}
+
+	return s
 }
 
-// LoadConfig loads in memory the server configuration from server.toml found in
+// loadConfig loads in memory the server configuration from server.toml found in
 // the static directory.
-func (s *Server) LoadConfig() error {
+func (s *Server) loadConfig() error {
 	log.Info("Loading config ...")
 
 	configFileName := filepath.Join(s.staticDir, "/server.toml")
@@ -71,11 +95,11 @@ func (s *Server) LoadConfig() error {
 	return nil
 }
 
-// LoadAreas loads all the areas from the static directory into memory.
+// loadAreas loads all the areas from the static directory into memory.
 // TODO: Change the way we load rooms into memory. We should load rooms
 // where online players are. We should also change our schema to hold
 // rooms in separate files.
-func (s *Server) LoadAreas() error {
+func (s *Server) loadAreas() error {
 	log.Info("Loading areas ...")
 	areaWalker := func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
@@ -104,6 +128,215 @@ func (s *Server) LoadAreas() error {
 	return filepath.Walk(s.staticDir+"/areas/", areaWalker)
 }
 
+func (s *Server) Start(port int64) {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Info(err.Error())
+		os.Exit(1)
+	}
+	log.Info(fmt.Sprintf("Listen on: %s", ln.Addr()))
+
+	wg := &sync.WaitGroup{}
+	quit := make(chan struct{})
+	regRequest := make(chan client.LoginRequest, 1000)
+	clientRequest := make(chan client.Request, 1000)
+
+	wg.Add(1)
+	go handleRegistrations(s, wg, quit, regRequest)
+
+	wg.Add(1)
+	go acceptConnections(ln, s, wg, quit, clientRequest, regRequest)
+
+	wg.Add(1)
+	go broadcast(s, wg, quit, clientRequest)
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, os.Kill)
+	select {
+	case <-signals:
+		log.Warn("Server is terminating...")
+		close(quit)
+	}
+
+	wg.Wait()
+	log.Warn("Server shutdown.")
+}
+
+// handleRegistrations accepts requests for registration and replies back if the requested
+// username exists or not.
+func handleRegistrations(s *Server, wg *sync.WaitGroup, quit chan struct{}, regRequest chan client.LoginRequest) {
+	log.Info("handleRegistrations started")
+	defer wg.Done()
+
+	for {
+		exists := false
+		var err error
+
+		select {
+		case <-quit:
+			log.Warn("handleRegistrations quit")
+			return
+		case request := <-regRequest:
+			exists, err = s.loadPlayer(request.Username)
+			if err != nil {
+				io.WriteString(request.Conn, fmt.Sprintf("%s\n", err.Error()))
+				continue
+			}
+
+			select {
+			case request.Reply <- exists:
+			case <-quit:
+				log.Warn("handleRegistrations quit")
+				return
+			}
+
+		}
+	}
+}
+
+func acceptConnections(
+	ln net.Listener,
+	s *Server,
+	wg *sync.WaitGroup,
+	quit <-chan struct{},
+	clientCh chan<- client.Request,
+	regRequest chan client.LoginRequest,
+) {
+	log.Info("acceptConnections started")
+	defer wg.Done()
+
+	for {
+		select {
+		case <-quit:
+			log.Warn("acceptConnections quit")
+			return
+		default:
+		}
+
+		ln.(*net.TCPListener).SetDeadline(time.Now().Add(10 * time.Second))
+		conn, err := ln.Accept()
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); !ok || !opErr.Timeout() {
+				log.Info(err.Error())
+			}
+			continue
+		}
+
+		// TODO: handleConnection is not terminating gracefully right now because it blocks on waiting
+		// ReadLinesInto to quit which in turn is blocked on user input.
+		go handleConnection(conn, s, wg, quit, clientCh, regRequest)
+	}
+}
+
+// handleConnection should be invoked as a goroutine.
+func handleConnection(
+	conn net.Conn,
+	s *Server,
+	wg *sync.WaitGroup,
+	quit <-chan struct{},
+	clientCh chan<- client.Request,
+	regRequest chan<- client.LoginRequest,
+) {
+	log.Info("handleConnection started")
+	defer wg.Done()
+
+	bufc := bufio.NewReader(conn)
+	defer conn.Close()
+
+	log.Info(fmt.Sprintf("New connection open: %s", conn.RemoteAddr()))
+
+	io.WriteString(conn, welcomePage)
+
+	var username string
+	questions := 0
+
+out:
+	for {
+		if questions >= 3 {
+			io.WriteString(conn, "See you\n")
+			return
+		}
+
+		username = promptMessage(conn, bufc, "Whats your Nick?\n")
+		isValidName := IsValidUsername(username)
+		if !isValidName {
+			questions++
+			io.WriteString(conn, fmt.Sprintf("Username %s is not valid (0-9a-z_-).\n", username))
+			continue
+		}
+
+		exists := false
+		replyCh := make(chan bool, 1)
+
+		select {
+		case regRequest <- client.LoginRequest{Username: username, Conn: conn, Reply: replyCh}:
+		case <-quit:
+			return
+		}
+
+		select {
+		case exists = <-replyCh:
+		case <-quit:
+			return
+		}
+
+		if exists {
+			break out
+		}
+
+		questions++
+		io.WriteString(conn, fmt.Sprintf("Username %s does not exists.\n", username))
+		answer := promptMessage(conn, bufc, "Do you want to create that user? [y|n] ")
+
+		if answer == "y" || answer == "yes" {
+			s.CreatePlayer(username)
+			break
+		}
+	}
+
+	player, _ := s.GetPlayerByNick(username)
+	c := client.NewClient(conn, &player, clientCh)
+	log.Info(fmt.Sprintf("Player %q got connected", c.Player.Nickname))
+	s.clientLoggedIn(c.Player.Nickname, *c)
+
+	wg.Add(1)
+	go c.Redraw(wg, quit)
+
+	// TODO: Main client thread is not terminating gracefully right now because it blocks on waiting
+	// for the user to hit Enter before proceeding to check for quit.
+	c.ReadLinesInto(quit)
+	log.Info(fmt.Sprintf("Connection from %v closed.", conn.RemoteAddr()))
+}
+
+func promptMessage(c net.Conn, bufc *bufio.Reader, message string) string {
+	for {
+		io.WriteString(c, message)
+		answer, _, _ := bufc.ReadLine()
+		if string(answer) != "" {
+			return string(answer)
+		}
+	}
+}
+
+// TODO: Maybe parallelize this so that each client request is handled on a separate routine.
+func broadcast(s *Server, wg *sync.WaitGroup, quit <-chan struct{}, reqChan <-chan client.Request) {
+	log.Info("broadcast started")
+	defer wg.Done()
+
+	wg.Add(1)
+	go God(s, wg, quit)
+
+	for {
+		select {
+		case request := <-reqChan:
+			s.HandleCommand(*request.Client, request.Cmd)
+		case <-quit:
+			log.Warn("broadcast quit")
+			return
+		}
+	}
+}
+
 func (s *Server) getPlayerFileName(playerName string) (bool, string) {
 	if !IsValidUsername(playerName) {
 		return false, ""
@@ -123,8 +356,8 @@ func IsValidUsername(playerName string) bool {
 	return true
 }
 
-// LoadPlayer loads the player into memory.
-func (s *Server) LoadPlayer(playerName string) (bool, error) {
+// loadPlayer loads the player into memory.
+func (s *Server) loadPlayer(playerName string) (bool, error) {
 	ok, playerFileName := s.getPlayerFileName(playerName)
 	if !ok {
 		return false, nil
@@ -166,7 +399,7 @@ func (s *Server) CreatePlayer(nick string) {
 	}
 	if _, err := os.Stat(playerFileName); err == nil {
 		log.Info(fmt.Sprintf("Player %q does already exist.\n", nick))
-		if _, err := s.LoadPlayer(nick); err != nil {
+		if _, err := s.loadPlayer(nick); err != nil {
 			log.Info(fmt.Sprintf("Player %q cannot be loaded: %v", nick, err))
 		}
 		return
@@ -182,10 +415,10 @@ func (s *Server) CreatePlayer(nick string) {
 	s.Players[player.Nickname] = player
 }
 
-// SavePlayer saves the player back to the static directory.
+// savePlayer saves the player back to the static directory.
 // TODO: Add an autosave mechanism instead of saving Players
 // once they quit.
-func (s *Server) SavePlayer(player area.Player) bool {
+func (s *Server) savePlayer(player area.Player) bool {
 	data := &bytes.Buffer{}
 	encoder := toml.NewEncoder(data)
 	err := encoder.Encode(player)
@@ -207,21 +440,21 @@ func (s *Server) SavePlayer(player area.Player) bool {
 
 // OnExit is a handler run by the server every time a player quits.
 func (s *Server) OnExit(client client.Client) {
-	s.SavePlayer(*client.Player)
-	s.ClientLoggedOut(client.Player.Nickname)
+	s.savePlayer(*client.Player)
+	s.clientLoggedOut(client.Player.Nickname)
 }
 
-// ClientLoggedIn stores the logged in player into an internal cache that holds
+// clientLoggedIn stores the logged in player into an internal cache that holds
 // all online players.
-func (s *Server) ClientLoggedIn(name string, client client.Client) {
+func (s *Server) clientLoggedIn(name string, client client.Client) {
 	s.Lock()
 	s.onlineClients[name] = &client
 	s.Unlock()
 }
 
-// ClientLoggedOut removes the logged out player from the internal cache that
+// clientLoggedOut removes the logged out player from the internal cache that
 // holds all online players.
-func (s *Server) ClientLoggedOut(name string) {
+func (s *Server) clientLoggedOut(name string) {
 	s.Lock()
 	delete(s.onlineClients, name)
 	s.Unlock()
