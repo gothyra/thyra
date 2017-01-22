11664b6c6d4f965db1e457484848fe26e644f7d2
Nikolaos Leandros Moraitis <droslean@gmail.com>
2017-01-22 01:21:07 +0100

    Prompted bar finished.

--- server/player.go
+++ server/player.go
@@ -22,6 +22,7 @@ const (
 	ENTER_KEY     = 13
 	SPACE_KEY     = 32
 	BACKSPACE_KEY = 127
+	DELETE_KEY    = 27
 )
 
 const (
@@ -100,8 +101,8 @@ type Player struct {
 	commandHistory       []string
 	rollback             int
 	wantHistory          bool
-	fromHistoryToCmd     string
 	finalCmd             string
+	position             int
 }
 
 // NewPlayer returns an initialized Player.
@@ -122,6 +123,7 @@ func NewPlayer(id ID, sshName, name, hash string, conn ssh.Channel) *Player {
 		commandHistory: make([]string, 1),
 		rollback:       0,
 		wantHistory:    false,
+		position:       0,
 	}
 	return p
 }
@@ -154,9 +156,10 @@ func (p *Player) resizeWatch() {
 	}
 }
 
+// TODO : Add special characters
 func (p *Player) promptBar(s *Server) {
-
 	buff := make([]byte, 3)
+
 	for {
 		//log.Debug(fmt.Sprintf("read buff is : %v", buff))
 		n, err := p.conn.Read(buff)
@@ -181,21 +184,26 @@ func (p *Player) promptBar(s *Server) {
 
 			// We use ARROW_UP to go back in command history.
 			case c == ARROW_UP:
-				//cursorBehavor = []byte{ansi.Esc, 91, 65}
 				p.wantHistory = true
 				p.arrowUp()
 
 			// We use ARROW_DOWN to go forward in command history.
 			case c == ARROW_DOWN:
-				//cursorBehavor = []byte{ansi.Esc, 91, 66}
 				p.wantHistory = true
 				p.arrowDown()
 
-			// TODO : User right -left to insert - delete the p.command
+			// We use ARROW_RIGHT to move right through the command for backspace and delete purpose.
 			case c == ARROW_RIGHT:
-				cursorBehavor = []byte{ansi.Esc, 91, 67}
+				if len(p.command) > p.position {
+					cursorBehavor = []byte{ansi.Esc, 91, 67}
+					p.position++
+				}
+			// We use ARROW_RIGHT to move left through the command for backspace and delete purpose.
 			case c == ARROW_LEFT:
-				cursorBehavor = []byte{ansi.Esc, 91, 68}
+				if len(p.command) > 0 {
+					cursorBehavor = []byte{ansi.Esc, 91, 68}
+					p.position--
+				}
 			}
 			p.conn.Write(cursorBehavor)
 		} else {
@@ -203,46 +211,70 @@ func (p *Player) promptBar(s *Server) {
 		}
 
 		switch n := b[0]; {
-
 		// Check uppercase letters
 		case n >= UPPER_ALPHA && n <= UPPER_OMEGA:
 			num := b[0] - 65
 			p.conn.Write([]byte(strings.ToUpper(alphabet[num])))
 			p.command = append(p.command, strings.ToUpper(alphabet[num]))
+			p.position++
 
 		// Check for lowercase letters
 		case n >= LOW_ALPHA && n <= LOW_OMEGA:
 			num := b[0] - 97
 			p.conn.Write([]byte(alphabet[num]))
 			p.command = append(p.command, alphabet[num])
+			p.position++
 
 		// Check for numbers
 		case n >= NUM_0 && n <= NUM_9:
 			num := b[0] - 48
 			p.conn.Write([]byte(fmt.Sprintf("%d", num)))
 			p.command = append(p.command, fmt.Sprintf("%d", num))
+			p.position++
 
 		// Enter key
 		case n == ENTER_KEY:
 			p.enterKey(s)
 		// Space key
 		case n == SPACE_KEY:
-			p.conn.Write([]byte(" "))
-			p.command = append(p.command, " ")
+			p.position++
+			if p.position < len(p.command) {
+				p.command = InsertInSlice(p.command, p.position-1, " ")
+				p.conn.Write(ansi.EraseLine)
+				p.conn.Write(ansi.Goto(uint16(p.h)-1, 1))
+				p.conn.Write([]byte(p.getCommandAsString()))
+				p.conn.Write(ansi.Goto(uint16(p.h)-1, uint16(p.position)+1))
+
+			} else {
+				p.conn.Write([]byte(" "))
+				p.command = append(p.command, " ")
+			}
 
 		// Backspace key
-		// TODO : Backspace is deleting the p.command. Make this work.!
 		case n == BACKSPACE_KEY:
-			backSpace := []byte{27, 91, 68}
-			p.conn.Write([]byte("\b "))
-			p.conn.Write([]byte(backSpace))
+			if p.position > 0 {
+				p.deletePartofCommand(p.position - 1)
+				p.position--
+				p.clearPromptBar()
+				p.conn.Write([]byte(p.getCommandAsString()))
+				p.conn.Write(ansi.Goto(uint16(p.h)-1, uint16(p.position)+1))
+			}
+			// Delete Key
+		case n == DELETE_KEY && b[2] == 51:
+			if p.position < len(p.command) {
+				p.deletePartofCommand(p.position)
+
+				p.clearPromptBar()
+				p.conn.Write([]byte(p.getCommandAsString()))
+				p.conn.Write(ansi.Goto(uint16(p.h)-1, uint16(p.position)+1))
+			}
 
 		//  Key ] only for debuging purpose.
 		case n == 93:
-			p.drawPromptBar()
 			log.Info(fmt.Sprintf("%#v", p.commandHistory))
 			log.Info(fmt.Sprintf("%#v", p.command))
 		}
+		//log.Debug(fmt.Sprintf("Position %d", p.position))
 	}
 }
 
@@ -252,6 +284,12 @@ func (p *Player) getCommandAsString() string {
 		cmd += p.command[i]
 	}
 	return cmd
+}
+
+func (p *Player) convertCommadHistoryToArray(command string) {
+	for i := 0; i < len(command); i++ {
+		p.command = append(p.command, string(command[i]))
+	}
 }
 
 func (p *Player) fillPromptBar() string {
@@ -268,24 +306,26 @@ func (p *Player) drawPromptBar() {
 	p.conn.Write(ansi.Goto(uint16(p.h)-1, 1))
 }
 
-// TODO : Split the string from commandHistory to p.command array
+// Travel backwards through the history of commands
 func (p *Player) arrowUp() {
-
-	p.conn.Write(ansi.EraseLine)
-	p.conn.Write(ansi.Goto(uint16(p.h-1), 1))
-
+	p.clearPromptBar()
 	p.rollback++
 
 	log.Debug(fmt.Sprintf("Len %d , Rollback %d", len(p.commandHistory), p.rollback))
 	if len(p.commandHistory)-p.rollback > 0 {
+		// Clear command array to re-use it again.
+		p.command = []string{}
 		if len(p.commandHistory)-p.rollback == 1 {
 			p.rollback--
 			p.conn.Write([]byte(p.commandHistory[1]))
-			p.fromHistoryToCmd = p.commandHistory[1]
+			p.convertCommadHistoryToArray(p.commandHistory[1])
+			p.position = len(p.command)
 
 		} else if len(p.commandHistory)-p.rollback > 1 {
 			p.conn.Write([]byte(p.commandHistory[len(p.commandHistory)-p.rollback]))
-			p.fromHistoryToCmd = p.commandHistory[len(p.commandHistory)-p.rollback]
+			p.convertCommadHistoryToArray(p.commandHistory[len(p.commandHistory)-p.rollback])
+			p.position = len(p.command)
+
 		}
 	} else {
 		log.Debug("No command history")
@@ -294,22 +334,24 @@ func (p *Player) arrowUp() {
 	}
 }
 
-// TODO : Split the string from commandHistory to p.command array
+// Travel forwards through the history of commands
 func (p *Player) arrowDown() {
-
-	p.conn.Write(ansi.EraseLine)
-	p.conn.Write(ansi.Goto(uint16(p.h-1), 1))
+	p.clearPromptBar()
 	p.rollback--
 
 	log.Debug(fmt.Sprintf("Len %d , Rollback %d", len(p.commandHistory), p.rollback))
 	if len(p.commandHistory)-p.rollback < len(p.commandHistory) {
+		// Clear command array to re-use it again.
+		p.command = []string{}
 		if len(p.commandHistory)-p.rollback == 1 {
 			p.rollback++
 			p.conn.Write([]byte(p.commandHistory[1]))
-			p.fromHistoryToCmd = p.commandHistory[1]
+			p.convertCommadHistoryToArray(p.commandHistory[1])
+			p.position = len(p.command)
 		} else {
 			p.conn.Write([]byte(p.commandHistory[len(p.commandHistory)-p.rollback]))
-			p.fromHistoryToCmd = p.commandHistory[len(p.commandHistory)-p.rollback]
+			p.convertCommadHistoryToArray(p.commandHistory[len(p.commandHistory)-p.rollback])
+			p.position = len(p.command)
 		}
 	} else {
 		log.Debug("No command history")
@@ -325,17 +367,10 @@ func (p *Player) enterKey(s *Server) {
 	p.conn.Write(ansi.Goto(uint16(p.h)-1, 1))
 	p.conn.Write(ansi.CursorHide)
 
-	if p.wantHistory && p.fromHistoryToCmd != "" {
-		p.wantHistory = false
-		p.finalCmd = p.fromHistoryToCmd
-
-	} else {
-		p.finalCmd = p.getCommandAsString()
-	}
-	p.commandHistory = append(p.commandHistory, p.finalCmd)
+	p.commandHistory = append(p.commandHistory, p.getCommandAsString())
 
 	for _, onlineClient := range s.onlinePlayers {
-		onlineClient.conn.Write([]byte(string(ansi.Goto(uint16(onlineClient.h-onlineClient.h+s.lines), 1)) + p.Name + " : " + p.finalCmd))
+		onlineClient.conn.Write([]byte(string(ansi.Goto(uint16(onlineClient.h-onlineClient.h+s.lines), 1)) + p.Name + " : " + p.getCommandAsString()))
 		onlineClient.conn.Write(ansi.Goto(uint16(onlineClient.h+s.lines), 1))
 	}
 
@@ -347,4 +382,32 @@ func (p *Player) enterKey(s *Server) {
 	s.Events <- event
 	// Clear command array to re-use it again.
 	p.command = []string{}
+
+	// Clear position
+	p.position = 0
+}
+
+func (p *Player) deletePartofCommand(position int) {
+	p.command = append(p.command[:position], p.command[position+1:]...)
+}
+
+func InsertInSlice(original []string, position int, value string) []string {
+	//we'll grow by 1
+	target := make([]string, len(original)+1)
+
+	//copy everything up to the position
+	copy(target, original[:position])
+
+	//set the new value at the desired position
+	target[position] = value
+
+	//copy everything left over
+	copy(target[position+1:], original[position:])
+
+	return target
+}
+
+func (p *Player) clearPromptBar() {
+	p.conn.Write(ansi.EraseLine)
+	p.conn.Write(ansi.Goto(uint16(p.h)-1, 1))
 }
